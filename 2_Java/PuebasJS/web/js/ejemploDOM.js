b240bae953dcd60216a726149c3af5f29a5c3fbf
Equipo <daniel.ruedalao@gmail.com>
2019-04-10 21:21:38 +0200

    hhh

--- 2_Java/PuebasJS/web/js/ejemploDOM.js
+++ 2_Java/PuebasJS/web/js/ejemploDOM.js
@@ -48,6 +48,61 @@ $(".botonReiniciar").click(function(){
 
 //AÑADIR ELEMENTOS
 jQuery(".botonAnadir").click(function(){  
-    i = i +1;
+    i = i + 1;
     jQuery("ol").append("<li>Núm: "+ i +"</i>").addClass("unaClase");    
 });
+
+//CALLS FUNCTIONS -----------------------------------------
+jQuery(document).ready(colorLista);
+jQuery(document).ready(pulsarBoton);
+jQuert(document).ready(ocultarTabla);
+
+//FUNCTIONS -----------------------------------------------
+function colorLista() {
+    $(".lista").mouseover(function(){
+        $(".lista").css("color","#FFFFFF");
+        $(".lista").css("background-color","#000000");
+    });
+    
+    $(".lista").mouseout(function(){
+        $(".lista").css("color","#000000");
+        $(".lista").css("background-color","#FFFFFF");
+    });
+};
+
+function pulsarBoton() {
+    //BOTON SUMAR
+    $(".botonSumar").mouseover(function(){
+        $(".botonSumar").attr("value","Pulsar");
+    });
+    $(".botonSumar").mouseout(function(){
+        $(".botonSumar").attr("value","Sumar lista");
+    });
+    //BOTON DOBLAR 
+    $(".botonDoblar").mouseover(function(){
+        $(".botonDoblar").attr("value","Pulsar");
+    });
+    $(".botonDoblar").mouseout(function(){
+        $(".botonDoblar").attr("value","Doblar lista");
+    });
+    //BOTON AÑADIR
+    $(".botonAnadir").mouseover(function(){
+        $(".botonAnadir").attr("value","Pulsar");
+    });
+    $(".botonAnadir").mouseout(function(){
+        $(".botonAnadir").attr("value","Añadir elemento");
+    });
+    //BOTON LIMPIAR
+    $(".botonReiniciar").mouseover(function(){
+        $(".botonReiniciar").attr("value","Pulsar");
+    });
+    $(".botonReiniciar").mouseout(function(){
+        $(".botonReiniciar").attr("value","Limpiar lista");
+    });
+};
+
+function ocultarTabla() {
+    $(".botonOcultar").click(function(){
+       $(".tabla").hide(); 
+    });
+};
